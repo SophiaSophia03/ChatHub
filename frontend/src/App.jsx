fd186f50d849b6d6232cbb16e99ebed5b44f8dcd
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-12-18 13:07:41 -0600

    Router setup for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Signup from './pages/Signup/Signup'
+import Login from './pages/Login/Login'
+import Profile from './pages/Profile/Profile'
+import Setting from './pages/SettingPage/Setting'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/signup" element={<Signup />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/profile" element={<Profile />}></Route>
+          <Route exact path="/setting" element={<Setting />}></Route>
+        </Routes>
+
+      <Footer />
+    </div>
   )
 }
 
