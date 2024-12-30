e1eb8e5a7d10342d18961b4663577283bf5e4dff
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-12-30 16:06:44 -0600

    Fixed the application for production mode

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from 'express';
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
@@ -10,6 +11,7 @@ import {app,server } from "./lib/socket.js"
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirName = path.resolve();
 
 // Middleware to parse JSON
 app.use(express.json({ limit: "50mb" }));
@@ -23,6 +25,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/msg", msgRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirName, "../frontend/dist")));
+
+  app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirName, "../frontend", "dist", "index.html"))
+  })
+}
 
 
 server.listen(PORT, () => {
